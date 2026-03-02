2fa59b7290cc5259b927beee3c37693731d926bf
SaadTAr <Saadalatar3445@gmail.com>
2026-03-02 18:50:45 +0000

    Add print_line prototype

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,12 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void more_numbers(void);
-void print_most_numbers(void);
-void print_numbers(void);
-int mul(int a, int b);
-int _isdigit(int c);
-int _isupper(int c);
 int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
 
 #endif
