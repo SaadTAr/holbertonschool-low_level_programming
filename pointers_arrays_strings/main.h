6e35f8963a66c15b2f36d23ae66968497977b131
SaadTAr <saadalatar3445@gmail.com>
2026-03-04 18:46:45 +0000

    Add _strcpy function

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+char *_strcpy(char *dest, char *src);
+oid print_array(int *a, int n);
+void puts_half(char *str);
 void puts2(char *str);
 void rev_string(char *s);
 void print_rev(char *s);
