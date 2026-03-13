5592134f67b3e6d5e9f471b094843c6439ffc90d
SaadTAr <saadalatar3445@gmail.com>
2026-03-13 17:55:53 +0000

    fix betty style

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,29 +1,30 @@
 #include "main.h"
 
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to search
+ * @accept: accepted bytes
+ * Return: number of bytes in the initial segment
+ */
+
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int count = 0;
-    int i;
-    int found;
+	unsigned int count = 0;
+	int i;
 
-    while (s[count] != '\0')
-    {
-        found = 0;
+	while (s[count] != '\0')
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (s[count] == accept[i])
+				break;
+		}
 
-        for (i = 0; accept[i] != '\0'; i++)
-        {
-            if (s[count] == accept[i])
-            {
-                found = 1;
-                break;
-            }
-        }
+		if (accept[i] == '\0')
+			break;
 
-        if (found == 0)
-            break;
+		count++;
+	}
 
-        count++;
-    }
-
-    return (count);
+	return (count);
 }
