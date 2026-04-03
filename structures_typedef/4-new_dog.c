eaa391a0ef2a55d7bcf7336d3cabf7cd6cb996aa
SaadTAr <saadalatar3445@gmail.com>
2026-04-03 19:15:55 +0000

    task 4 new_dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Return: pointer to new dog, or NULL if fail
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
+
+	d->name = strdup(name);
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+
+	d->owner = strdup(owner);
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+
+	d->age = age;
+
+	return (d);
+}
