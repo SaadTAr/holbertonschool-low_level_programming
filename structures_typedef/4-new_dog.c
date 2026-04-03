f91124e41e1ca6460b7a7b825d7492a29a79b444
SaadTAr <saadalatar3445@gmail.com>
2026-04-03 19:19:03 +0000

    fix task 4 new_dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,40 @@
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
+
+/**
+ * _strlen - returns length of string
+ * @s: string
+ *
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+
+	return (len);
+}
+
+/**
+ * _strcpy - copies string
+ * @dest: destination
+ * @src: source
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	dest[i] = '\0';
+	return (dest);
+}
 
 /**
  * new_dog - creates a new dog
@@ -14,18 +48,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
 
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
 
-	d->name = strdup(name);
+	d->name = malloc(sizeof(char) * (_strlen(name) + 1));
 	if (d->name == NULL)
 	{
 		free(d);
 		return (NULL);
 	}
 
-	d->owner = strdup(owner);
+	d->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 	if (d->owner == NULL)
 	{
 		free(d->name);
@@ -33,6 +70,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
+	_strcpy(d->name, name);
+	_strcpy(d->owner, owner);
 	d->age = age;
 
 	return (d);
