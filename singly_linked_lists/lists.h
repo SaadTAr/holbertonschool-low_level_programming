c1eecbc9fba2a92733aa8bd5853082a20dc9cba6
SaadTAr <saadalatar3445@gmail.com>
2026-04-25 18:04:13 +0000

    fix betty lists.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,13 +1,19 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
+#include <stdlib.h>
 
+/**
+ * struct list_s - singly linked list
+ * @str: string
+ * @len: length of string
+ * @next: points to next node
+ */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
