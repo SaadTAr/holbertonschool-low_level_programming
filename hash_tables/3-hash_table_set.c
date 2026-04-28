7e30b80174d256f47302b8941b321fd3e6bd4b1f
SaadTAr <saadalatar3445@gmail.com>
2026-04-28 12:21:31 +0000

    Fix Betty style

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -3,59 +3,72 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - adds an element to the hash table
- * @ht: hash table
- * @key: key
- * @value: value
- *
- * Return: 1 if success, 0 if fail
+ * update_node - update value if key exists
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+int update_node(hash_node_t *node, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *node, *tmp;
-	char *val_copy;
+	char *val;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-		return (0);
-
-	index = key_index((const unsigned char *)key, ht->size);
-
-	tmp = ht->array[index];
-
-	/* update if key exists */
-	while (tmp)
+	while (node)
 	{
-		if (strcmp(tmp->key, key) == 0)
+		if (strcmp(node->key, key) == 0)
 		{
-			val_copy = strdup(value);
-			if (val_copy == NULL)
+			val = strdup(value);
+			if (!val)
 				return (0);
-
-			free(tmp->value);
-			tmp->value = val_copy;
+			free(node->value);
+			node->value = val;
 			return (1);
 		}
-		tmp = tmp->next;
+		node = node->next;
 	}
+	return (0);
+}
 
-	/* create new node */
+/**
+ * create_node - create new node
+ */
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *node;
+
 	node = malloc(sizeof(hash_node_t));
-	if (node == NULL)
-		return (0);
+	if (!node)
+		return (NULL);
 
 	node->key = strdup(key);
 	node->value = strdup(value);
 
-	if (node->key == NULL || node->value == NULL)
+	if (!node->key || !node->value)
 	{
 		free(node->key);
 		free(node->value);
 		free(node);
-		return (0);
+		return (NULL);
 	}
+	return (node);
+}
 
-	/* insert at beginning */
+/**
+ * hash_table_set - adds element
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (!ht || !key || *key == '\0' || !value)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	if (update_node(ht->array[index], key, value))
+		return (1);
+
+	node = create_node(key, value);
+	if (!node)
+		return (0);
+
 	node->next = ht->array[index];
 	ht->array[index] = node;
 
